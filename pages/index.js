a04e623109e25d3664b86639097a43873e8ed82b
jillnguyen2311 <nguyennuduyhoai75@gmail.com>
2023-03-16 23:11:51 -0700

    fix errors language

--- pages/index.js
+++ pages/index.js
@@ -28,20 +28,20 @@ export default function Home({ dir }) {
 
       <nav className={styles.nav}>
         <div className={styles.topNav}>
-          <div>LEARNING HUB</div>
-          <div>MYBCIT</div>
-          <div>STUDENTS</div>
-          <div>STAFF</div>
-          <div>EVENTS</div>
+          <div><FormattedMessage id="nav.learninghub" /></div>
+          <div><FormattedMessage id="nav.mybcit" /></div>
+          <div><FormattedMessage id="nav.students" /></div>
+          <div><FormattedMessage id="nav.staff" /></div>
+          <div><FormattedMessage id="nav.events" /></div>
         </div>
 
         <div className={styles.mainNav}>
           <img src="https://issp.bcit.ca/BCIT_logo.png" width={80} height={80} />
-          <div>Program & Courses</div>
-          <div>Admission</div>
-          <div>Student Services</div>
-          <div>Research</div>
-          <div>About BCIT</div>
+          <div><FormattedMessage id="nav.procourse" /></div>
+          <div><FormattedMessage id="nav.admission" /></div>
+          <div><FormattedMessage id="nav.services" /></div>
+          <div><FormattedMessage id="nav.research" /></div>
+          <div><FormattedMessage id="nav.about" /></div>
 
           <div>
             <form className={styles.searchbox}>
@@ -55,9 +55,9 @@ export default function Home({ dir }) {
       </nav>
 
       <div className={styles.underNav}>
-        <div>STUDY</div>
-        <div>▶ BUSINESS & MEDIA</div>
-        <div>▶ DIGITAL DESIGN AND DEVELOPMENT</div>
+        <div><FormattedMessage id="underNav.study" /></div>
+        <div><FormattedMessage id="underNav.department" /></div>
+        <div><FormattedMessage id="underNav.program" /></div>
       </div>
       
       <img src={image} className={styles.image}/>
@@ -74,8 +74,8 @@ export default function Home({ dir }) {
           </p>
 
           <header className={styles.lang}>
-          <h1>LANGUAGE: </h1>
-          <div>
+          <h1><FormattedMessage id="lang" /> </h1>
+          <div className={styles.langconvert}>
             {[...locales].sort().map((locale) => (
               <Link key={locale} href="/" locale={locale}>
                 <div>{locale}</div>
@@ -89,99 +89,99 @@ export default function Home({ dir }) {
         <div className={styles.mainBody}>
           <div className={styles.leftSide}>
             <div>
-              <h3 className={styles.lefth2}>ABOUT THIS PROGRAM</h3>
+              <h3 className={styles.lefth2}><FormattedMessage id="main.left.aboutProgram" /></h3>
               <div className={styles.list}>
-                <p>Overview</p>
-                <p>Entrance Requirements</p>
-                <p>Advanced Placement</p>
-                <p>Costs & Supplies</p>
-                <p>Courses</p>
-                <p>Program Details</p>
-                <p className={styles.linking}>Graduating & Jobs</p>
-                <p>Contact Us</p>
-                <p>Subscribe</p>
+                <p><FormattedMessage id="main.left.aboutProgram.list1" /></p>
+                <p><FormattedMessage id="main.left.aboutProgram.list2" /></p>
+                <p><FormattedMessage id="main.left.aboutProgram.list3" /></p>
+                <p><FormattedMessage id="main.left.aboutProgram.list4" /></p>
+                <p><FormattedMessage id="main.left.aboutProgram.list5" /></p>
+                <p><FormattedMessage id="main.left.aboutProgram.list6" /></p>
+                <p className={styles.linking}><FormattedMessage id="main.left.aboutProgram.list7"/></p>
+                <p><FormattedMessage id="main.left.aboutProgram.list8" /></p>
+                <p><FormattedMessage id="main.left.aboutProgram.list9" /></p>
               </div>
             </div>
 
             <div>
-              <h2>Info sessions</h2>
+              <h2><FormattedMessage id="main.left.infoSessions" /></h2>
               <p>
-                Attend an upcoming information session for this program:
+              <FormattedMessage id="main.left.infoSessions.desc" />
               </p>
               <ul>
-                <li> <span className={styles.linking}>April 3rd at 5:30pm</span></li>
-                <li> <span className={styles.linking}>May 8th at 5:30pm</span></li>
-                <li> <span className={styles.linking}>June 19th at 5:30pm</span></li>
-                <li> <span className={styles.linking}>July 10th at 5:30pm</span></li>
+                <li> <span className={styles.linking}><FormattedMessage id="main.left.infoSessions.list1" /></span></li>
+                <li> <span className={styles.linking}><FormattedMessage id="main.left.infoSessions.list2" /></span></li>
+                <li> <span className={styles.linking}><FormattedMessage id="main.left.infoSessions.list3" /></span></li>
+                <li> <span className={styles.linking}><FormattedMessage id="main.left.infoSessions.list4" /></span></li>
               </ul>
-              <p>Explore all upcoming <span className={styles.linking}> BCIT info sessions.</span></p>
+              <p><FormattedMessage id="main.left.infoSessions.end1"/><span className={styles.linking}><FormattedMessage id="main.left.infoSessions.end2" /></span></p>
             </div>
 
             <div>
-              <h2>Program Overview</h2>
+              <h2><FormattedMessage id="main.left.progOverview"/></h2>
               <div className={styles.list}>
-                <p><b>Credential:</b> Diploma</p>
-                <p><b>Format:</b> Full-time</p>
-                <p><b>Length:</b> 2 years</p>
-                <p><b>Start Date:</b> September</p>
-                <p><b>Campus:</b> Burnaby</p>
-                <p><b>Domestic Tuition:</b> $18,000*</p>
-                <p><b>International Tuition:</b> $37,600*</p>
-                <p>*cost estimate</p>
+                <p><b><FormattedMessage id="main.left.overview.list1a" /></b><FormattedMessage id="main.left.overview.list1b" /></p>
+                <p><b><FormattedMessage id="main.left.overview.list2a" /></b><FormattedMessage id="main.left.overview.list2b" /></p>
+                <p><b><FormattedMessage id="main.left.overview.list3a" /></b><FormattedMessage id="main.left.overview.list3b" /></p>
+                <p><b><FormattedMessage id="main.left.overview.list4a" /></b><FormattedMessage id="main.left.overview.list4b" /></p>
+                <p><b><FormattedMessage id="main.left.overview.list5a" /></b><FormattedMessage id="main.left.overview.list5b" /></p>
+                <p><b><FormattedMessage id="main.left.overview.list6a" /></b><FormattedMessage id="main.left.overview.list6b" /></p>
+                <p><b><FormattedMessage id="main.left.overview.list7a" /></b><FormattedMessage id="main.left.overview.list7b" /></p>
+                <p><FormattedMessage id="main.left.overview.list8a" /></p>
               </div>
-              <p className={styles.linking}><b>All Business + Media Programs</b></p>
+              <p className={styles.linking}><b><FormattedMessage id="main.left.overview.end" /></b></p>
             </div>
 
             <div>
-              <h2>Recently visited programs</h2>
+              <h2><FormattedMessage id="main.left.recent"/></h2>
               <div className={styles.gap}>
-                <div><p className={styles.linking}>Digital Design and Development</p> <p>Diploma</p></div>
-                <div><p className={styles.linking}>Bachelor of Business Administration</p> <p>Bachelor of Business Administration</p></div>
-                <div><p className={styles.linking}>Computer Systems Technology</p> <p>Diploma</p></div>
-                <div><p className={styles.linking}>Computer Systems (Games Development Option)</p> <p>Bachelor of Technology</p></div>
+                <div><p className={styles.linking}><FormattedMessage id="main.left.recent.list1a" /></p> <p><FormattedMessage id="main.left.recent.list1b" /></p></div>
+                <div><p className={styles.linking}><FormattedMessage id="main.left.recent.list2a" /></p> <p><FormattedMessage id="main.left.recent.list2b" /></p></div>
+                <div><p className={styles.linking}><FormattedMessage id="main.left.recent.list3a" /></p> <p><FormattedMessage id="main.left.recent.list3b" /></p></div>
+                <div><p className={styles.linking}><FormattedMessage id="main.left.recent.list4a" /></p> <p><FormattedMessage id="main.left.recent.list4b" /></p></div>
               </div>
             </div>
           </div>
 
           <div className={styles.rightSide}>
-            <h1 className={styles.righth1}>Graduating & Jobs </h1>
-            <p>Graduates have a variety of digital media skills that can lead them to various content design and development jobs with digital media production studios, advertising companies, design firms, and web and mobile app developers. Graduates may also have opportunities to be part of a team that creates games, digital animations, and dynamic content.</p>
-            <p>Job opportunities:</p>
+            <h1 className={styles.righth1}><FormattedMessage id="main.right.graduate"/></h1>
+            <p><FormattedMessage id="main.right.graduate.desc"/></p>
+            <p><FormattedMessage id="main.right.graduate.job"/></p>
             <ul>
-              <li>Mobile and Web Content Artist</li>
-              <li>Internet Marketing Coordinator</li>
-              <li>Digital Media Coordinator</li>
-              <li>Web and Mobile Developer</li>
-              <li>Interface Designer</li>
-              <li>Game User Interface Designer</li>
-              <li>Project Manager</li>
-              <li>Digital Media Manager</li>
-              <li>Digital Media Producer</li>
-              <li>Web Development Manager</li>
-              <li>Dynamic Media Designer</li>
-              <li>Digital Media Product Designer</li>
+              <li><FormattedMessage id="main.right.graduate.job.list1" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list2" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list3" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list4" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list5" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list6" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list7" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list8" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list9" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list10" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list11" /></li>
+              <li><FormattedMessage id="main.right.graduate.job.list12" /></li>
             </ul>
 
             <div>
-              <h2 className={styles.righth2}>Graduate employment outcomes</h2>
-              <p>The BCIT student outcomes report presents summary findings from the annual survey of former students administered by BC Stats one to two years after graduation. These reports combine the last three years of available results for the 2020-2022 BCIT Outcomes Surveys of 2019-2021 graduates and for Degree 2017-2019 graduates. The reports are organized into three-page summaries containing information on graduates’ labour market experiences and opinions regarding their education. More detailed information can be accessed at the <span className={styles.underlineLink}>BC Student Outcomes</span> website.</p>
-              <p>To view these results, you may need to have the <span className={styles.underlineLink}>Adobe Acrobat Reader</span> installed in your Web browser.</p>
+              <h2 className={styles.righth2}><FormattedMessage id="main.right.outcome" /></h2>
+              <p><FormattedMessage id="main.right.outcome.summary" /><span className={styles.underlineLink}><FormattedMessage id="main.right.outcome.summary.a" /></span></p>
+              <p><FormattedMessage id="main.right.result" /><span className={styles.underlineLink}><FormattedMessage id="main.right.result.a" /></span><FormattedMessage id="main.right.result.b" /></p>
               <ul>
-                <li><span className={styles.underlineLink}>Digital Design and Development</span></li>
+                <li><span className={styles.underlineLink}><FormattedMessage id="main.right.underlink" /></span></li>
               </ul>
 
               <div className={styles.backandnext}>
                 <div>
-                  <p className={styles.rightp}>Go back</p>
-                  <span className={styles.boldUnderline}>PROGRAM DETAILS</span>
+                  <p className={styles.rightp}><FormattedMessage id="main.right.goback" /></p>
+                  <span className={styles.boldUnderline}><FormattedMessage id="main.right.progDetails" /></span>
                 </div>
 
                 <div>
-                  <p className={styles.rightp}>Next up</p>
-                  <span className={styles.boldUnderline}>CONTACT US</span>
+                  <p className={styles.rightp}><FormattedMessage id="main.right.nextup" /></p>
+                  <span className={styles.boldUnderline}><FormattedMessage id="main.right.contact" /></span>
                 </div>
                 </div>
-                <p className={styles.lastLine}>Programs and courses are subject to change without notice.</p>
+                <p className={styles.lastLine}><FormattedMessage id="main.right.end" /></p>
 
               
             </div>
@@ -191,50 +191,50 @@ export default function Home({ dir }) {
 
       <footer class={styles.footer}>
         <div class={styles.col1}>
-          <h3 className={styles.footerTitle}><span className={styles.beforeLine}>____</span>CONTACT</h3>
+          <h3 className={styles.footerTitle}><span className={styles.beforeLine}>____</span><FormattedMessage id="footer.col1.contact" /></h3>
           <div className={styles.footerpSmall}>
-            <div>3700 Willingdon Ave.</div>
-            <div>Burnaby, British Columbia</div>
+            <div><FormattedMessage id="footer.col1.contact.addr1" /></div>
+            <div><FormattedMessage id="footer.col1.contact.addr2" /></div>
             <div>Canada V5G 3H2</div>
             <div className={styles.linking}><b>bcit.ca</b></div>
           </div>
           <div className={styles.footerpSmall}>
-            <div>Telephone: 604-434-5734</div>
-            <div>Toll-free (Can/US): 1-866-434-1610</div>
-            <div className={styles.linking}><b>More contact numbers</b></div>
+            <div><FormattedMessage id="footer.col1.contact.phone" /></div>
+            <div><FormattedMessage id="footer.col1.contact.phone.toll" /></div>
+            <div className={styles.linking}><b><FormattedMessage id="footer.col1.contact.more" /></b></div>
           </div>
           <img src='/socialmedia.png'/>
         </div>
 
         <div class={styles.col2}>
           <div class={styles.footerpLink}>
-            <div>Visit</div>
-            <div>Apply Now</div>
-            <div>Give</div>
+            <div><FormattedMessage id="footer.col2.visit" /></div>
+            <div><FormattedMessage id="footer.col2.apply" /></div>
+            <div><FormattedMessage id="footer.col2.give" /></div>
           </div>
         </div>
 
         <div class={styles.col3}>
           <div class={styles.footerpLink}>
-            <div>News</div>
-            <div>Events</div>
-            <div>Careers</div>
+            <div><FormattedMessage id="footer.col3.news" /></div>
+            <div><FormattedMessage id="footer.col3.events" /></div>
+            <div><FormattedMessage id="footer.col3.careers" /></div>
           </div>
         </div>
 
         <div class={styles.col4}>
-          <h3 className={styles.footerTitle}><span className={styles.beforeLine}>____</span> CAMPUS INFORMATION</h3>
+          <h3 className={styles.footerTitle}><span className={styles.beforeLine}>____</span><FormattedMessage id="footer.col4.campus" /></h3>
           <div class={styles.footerpLink}>
-            <div>Burnaby Campus</div>
-            <div>Downtown Campus</div>
-            <div>Marine Campus</div>
-            <div>Aerospace Technology Campus</div>
-            <div>Annacis Island Campus</div>
+            <div><FormattedMessage id="footer.col4.campus.burnaby" /></div>
+            <div><FormattedMessage id="footer.col4.campus.downtown" /></div>
+            <div><FormattedMessage id="footer.col4.campus.marine" /></div>
+            <div><FormattedMessage id="footer.col4.campus.aerospace" /></div>
+            <div><FormattedMessage id="footer.col4.campus.annacis" /></div>
           </div>
           <div className={styles.footerpSmallLink}>
-            <div>Feedback</div>
-            <div>Copyright</div>
-            <div>Privacy</div>
+            <div><FormattedMessage id="footer.col4.feedback" /></div>
+            <div><FormattedMessage id="footer.col4.copyright" /></div>
+            <div><FormattedMessage id="footer.col4.privacy" /></div>
           </div>
         </div>
       </footer>
